5a4912947540e08c5e8649ee4279c09c070e78ce
RGloza111 <118307804+RGloza111@users.noreply.github.com>
2023-05-18 00:15:41 +0300

    Create third.cs

---
+++ third.cs
@@ -0,0 +1,11 @@
+using System;
+namespace main1
+{
+    class Progrma
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Гльоза Р.Е., ФIТ, 121, денна, 1-3");
+        }
+    }
+}
